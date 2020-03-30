a2915b5b0ecc9fcfc116267985c741460a858e18
Nobiisme-JesPaPa <59558986+Nobiisme-JesPaPa@users.noreply.github.com>
2020-03-30 14:28:30 +0200

    Startet på Pizzaria
    Har lavet interface til pizzariaet.

---
+++ MaMaMiaPizzaria/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace MaMaMiaPizzaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KøbNu = new System.Windows.Forms.Button();
+            this.Bestilling = new System.Windows.Forms.Label();
+            this.BestillingPris = new System.Windows.Forms.TextBox();
+            this.kr = new System.Windows.Forms.Label();
+            this.Pizza1 = new System.Windows.Forms.Label();
+            this.Ingr1 = new System.Windows.Forms.Label();
+            this.Pizza2 = new System.Windows.Forms.Label();
+            this.Ingr2 = new System.Windows.Forms.Label();
+            this.Pizza3 = new System.Windows.Forms.Label();
+            this.Ingr3 = new System.Windows.Forms.Label();
+            this.Medium = new System.Windows.Forms.Label();
+            this.Stor = new System.Windows.Forms.Label();
+            this.Medium1 = new System.Windows.Forms.CheckBox();
+            this.Stor1 = new System.Windows.Forms.CheckBox();
+            this.Medium2 = new System.Windows.Forms.CheckBox();
+            this.Stor2 = new System.Windows.Forms.CheckBox();
+            this.Medium3 = new System.Windows.Forms.CheckBox();
+            this.Stor3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // KøbNu
+            // 
+            this.KøbNu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KøbNu.Location = new System.Drawing.Point(930, 90);
+            this.KøbNu.Name = "KøbNu";
+            this.KøbNu.Size = new System.Drawing.Size(157, 42);
+            this.KøbNu.TabIndex = 0;
+            this.KøbNu.Text = "Køb Nu";
+            this.KøbNu.UseVisualStyleBackColor = true;
+            // 
+            // Bestilling
+            // 
+            this.Bestilling.AutoSize = true;
+            this.Bestilling.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bestilling.Location = new System.Drawing.Point(926, 13);
+            this.Bestilling.Name = "Bestilling";
+            this.Bestilling.Size = new System.Drawing.Size(137, 24);
+            this.Bestilling.TabIndex = 1;
+            this.Bestilling.Text = "Din Bestilling:";
+            // 
+            // BestillingPris
+            // 
+            this.BestillingPris.Location = new System.Drawing.Point(930, 52);
+            this.BestillingPris.Name = "BestillingPris";
+            this.BestillingPris.Size = new System.Drawing.Size(115, 22);
+            this.BestillingPris.TabIndex = 2;
+            // 
+            // kr
+            // 
+            this.kr.AutoSize = true;
+            this.kr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kr.Location = new System.Drawing.Point(1051, 50);
+            this.kr.Name = "kr";
+            this.kr.Size = new System.Drawing.Size(36, 24);
+            this.kr.TabIndex = 3;
+            this.kr.Text = "Kr.";
+            // 
+            // Pizza1
+            // 
+            this.Pizza1.AutoSize = true;
+            this.Pizza1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza1.Location = new System.Drawing.Point(32, 25);
+            this.Pizza1.Name = "Pizza1";
+            this.Pizza1.Size = new System.Drawing.Size(78, 20);
+            this.Pizza1.TabIndex = 4;
+            this.Pizza1.Text = "1. Cihan";
+            // 
+            // Ingr1
+            // 
+            this.Ingr1.AutoSize = true;
+            this.Ingr1.Location = new System.Drawing.Point(33, 50);
+            this.Ingr1.Name = "Ingr1";
+            this.Ingr1.Size = new System.Drawing.Size(378, 21);
+            this.Ingr1.TabIndex = 5;
+            this.Ingr1.Text = "TomatSauce, Ost, Kylling, Løg og Champignon";
+            // 
+            // Pizza2
+            // 
+            this.Pizza2.AutoSize = true;
+            this.Pizza2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza2.Location = new System.Drawing.Point(32, 78);
+            this.Pizza2.Name = "Pizza2";
+            this.Pizza2.Size = new System.Drawing.Size(87, 20);
+            this.Pizza2.TabIndex = 6;
+            this.Pizza2.Text = "2. Titanic";
+            // 
+            // Ingr2
+            // 
+            this.Ingr2.AutoSize = true;
+            this.Ingr2.Location = new System.Drawing.Point(33, 98);
+            this.Ingr2.Name = "Ingr2";
+            this.Ingr2.Size = new System.Drawing.Size(475, 21);
+            this.Ingr2.TabIndex = 7;
+            this.Ingr2.Text = "TomatSauce, Ost, Kebab, Salat, Agurk, Tomat og Dressing";
+            // 
+            // Pizza3
+            // 
+            this.Pizza3.AutoSize = true;
+            this.Pizza3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza3.Location = new System.Drawing.Point(32, 131);
+            this.Pizza3.Name = "Pizza3";
+            this.Pizza3.Size = new System.Drawing.Size(87, 20);
+            this.Pizza3.TabIndex = 8;
+            this.Pizza3.Text = "3. Hawaii";
+            // 
+            // Ingr3
+            // 
+            this.Ingr3.AutoSize = true;
+            this.Ingr3.Location = new System.Drawing.Point(33, 151);
+            this.Ingr3.Name = "Ingr3";
+            this.Ingr3.Size = new System.Drawing.Size(300, 21);
+            this.Ingr3.TabIndex = 9;
+            this.Ingr3.Text = "TomatSauce, Ost, Skinke og Ananas";
+            // 
+            // Medium
+            // 
+            this.Medium.AutoSize = true;
+            this.Medium.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Medium.Location = new System.Drawing.Point(528, 4);
+            this.Medium.Name = "Medium";
+            this.Medium.Size = new System.Drawing.Size(74, 20);
+            this.Medium.TabIndex = 10;
+            this.Medium.Text = "Medium";
+            // 
+            // Stor
+            // 
+            this.Stor.AutoSize = true;
+            this.Stor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stor.Location = new System.Drawing.Point(668, 4);
+            this.Stor.Name = "Stor";
+            this.Stor.Size = new System.Drawing.Size(44, 20);
+            this.Stor.TabIndex = 11;
+            this.Stor.Text = "Stor";
+            // 
+            // Medium1
+            // 
+            this.Medium1.AutoSize = true;
+            this.Medium1.Location = new System.Drawing.Point(532, 27);
+            this.Medium1.Name = "Medium1";
+            this.Medium1.Size = new System.Drawing.Size(69, 26);
+            this.Medium1.TabIndex = 12;
+            this.Medium1.Text = "49,-";
+            this.Medium1.UseVisualStyleBackColor = true;
+            // 
+            // Stor1
+            // 
+            this.Stor1.AutoSize = true;
+            this.Stor1.Location = new System.Drawing.Point(672, 24);
+            this.Stor1.Name = "Stor1";
+            this.Stor1.Size = new System.Drawing.Size(69, 26);
+            this.Stor1.TabIndex = 13;
+            this.Stor1.Text = "69,-";
+            this.Stor1.UseVisualStyleBackColor = true;
+            // 
+            // Medium2
+            // 
+            this.Medium2.AutoSize = true;
+            this.Medium2.Location = new System.Drawing.Point(532, 77);
+            this.Medium2.Name = "Medium2";
+            this.Medium2.Size = new System.Drawing.Size(69, 26);
+            this.Medium2.TabIndex = 14;
+            this.Medium2.Text = "49,-";
+            this.Medium2.UseVisualStyleBackColor = true;
+            // 
+            // Stor2
+            // 
+            this.Stor2.AutoSize = true;
+            this.Stor2.Location = new System.Drawing.Point(672, 77);
+            this.Stor2.Name = "Stor2";
+            this.Stor2.Size = new System.Drawing.Size(69, 26);
+            this.Stor2.TabIndex = 15;
+            this.Stor2.Text = "69,-";
+            this.Stor2.UseVisualStyleBackColor = true;
+            // 
+            // Medium3
+            // 
+            this.Medium3.AutoSize = true;
+            this.Medium3.Location = new System.Drawing.Point(532, 133);
+            this.Medium3.Name = "Medium3";
+            this.Medium3.Size = new System.Drawing.Size(69, 26);
+            this.Medium3.TabIndex = 16;
+            this.Medium3.Text = "39,-";
+            this.Medium3.UseVisualStyleBackColor = true;
+            // 
+            // Stor3
+            // 
+            this.Stor3.AutoSize = true;
+            this.Stor3.Location = new System.Drawing.Point(672, 133);
+            this.Stor3.Name = "Stor3";
+            this.Stor3.Size = new System.Drawing.Size(69, 26);
+            this.Stor3.TabIndex = 17;
+            this.Stor3.Text = "59,-";
+            this.Stor3.UseVisualStyleBackColor = true;
+            this.Stor3.CheckedChanged += new System.EventHandler(this.Stor3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 450);
+            this.Controls.Add(this.Stor3);
+            this.Controls.Add(this.Medium3);
+            this.Controls.Add(this.Stor2);
+            this.Controls.Add(this.Medium2);
+            this.Controls.Add(this.Stor1);
+            this.Controls.Add(this.Medium1);
+            this.Controls.Add(this.Stor);
+            this.Controls.Add(this.Medium);
+            this.Controls.Add(this.Ingr3);
+            this.Controls.Add(this.Pizza3);
+            this.Controls.Add(this.Ingr2);
+            this.Controls.Add(this.Pizza2);
+            this.Controls.Add(this.Ingr1);
+            this.Controls.Add(this.Pizza1);
+            this.Controls.Add(this.kr);
+            this.Controls.Add(this.BestillingPris);
+            this.Controls.Add(this.Bestilling);
+            this.Controls.Add(this.KøbNu);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button KøbNu;
+        private System.Windows.Forms.Label Bestilling;
+        private System.Windows.Forms.TextBox BestillingPris;
+        private System.Windows.Forms.Label kr;
+        private System.Windows.Forms.Label Pizza1;
+        private System.Windows.Forms.Label Ingr1;
+        private System.Windows.Forms.Label Pizza2;
+        private System.Windows.Forms.Label Ingr2;
+        private System.Windows.Forms.Label Pizza3;
+        private System.Windows.Forms.Label Ingr3;
+        private System.Windows.Forms.Label Medium;
+        private System.Windows.Forms.Label Stor;
+        private System.Windows.Forms.CheckBox Medium1;
+        private System.Windows.Forms.CheckBox Stor1;
+        private System.Windows.Forms.CheckBox Medium2;
+        private System.Windows.Forms.CheckBox Stor2;
+        private System.Windows.Forms.CheckBox Medium3;
+        private System.Windows.Forms.CheckBox Stor3;
+    }
+}
+
